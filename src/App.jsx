5615987a8f3fc94bd39a78d32fc9d9a1fa98071d
Arunraja <12345678+arunraja124-stack@users.noreply.github.com>
2026-01-28 16:26:15 +0530

    Fix AuthPage import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
-import AuthPage from "./components/auth/AuthPage.jsx";
+import AuthPage from "./components/auth/Authpage";
+
 
 function App() {
   // 🔐 AUTH STATE
